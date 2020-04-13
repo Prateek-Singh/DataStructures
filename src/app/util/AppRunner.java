b3c29f4c815e4ee7004ab01740267422e28da11c
Prateek-Singh <prateek.singh@finicity.com>
2020-04-13 20:01:19 +0530

    Changed code to use Try with resources

--- src/app/util/AppRunner.java
+++ src/app/util/AppRunner.java
@@ -8,20 +8,24 @@ import java.util.Scanner;
  * @author Prateek Singh
  */
 public class AppRunner {
-    public static void run() throws Exception {        
-        while(true) {
-            System.out.println("Select the Id of the data structure to run or press 0 to exit");
-            for(DSInstances dsInstances : DSInstances.getList()) {
-                System.out.println(String.format("%d : %s", dsInstances.getId(), dsInstances.getName()));
+    public static void run() throws Exception {    
+        try(Scanner scanner = new Scanner(System.in)) {
+            while(true) {
+                System.out.println("Select the Id of the data structure to run or press 0 to exit");
+                for(DSInstances dsInstances : DSInstances.getList()) {
+                    System.out.println(String.format("%d : %s", dsInstances.getId(), dsInstances.getName()));
+                }
+                int id = scanner.nextInt();
+                if (id == 0) {
+                    break;
+                } else {
+                    Class dsToInstantiate = DSInstances.getClass(id);
+                    Runner run = (Runner) dsToInstantiate.newInstance();
+                    run.run();
+                }
             }
-            int id = new Scanner(System.in).nextInt();
-            if (id == 0) {
-                break;
-            } else {
-                Class dsToInstantiate = DSInstances.getClass(id);
-                Runner run = (Runner) dsToInstantiate.newInstance();
-                run.run();
-            }
-        }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        } 
     }
 }
